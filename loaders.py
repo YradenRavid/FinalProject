d182ba97da37561a1bb22d2f205954f592c10a65
YradenRavid <39432202+YradenRavid@users.noreply.github.com>
2018-05-20 12:41:08 +0300

    Add files via upload

---
+++ loaders.py
@@ -0,0 +1,284 @@
+import numpy as np
+import tensorflow as tf
+import utils
+import os
+import scipy.io
+import matplotlib.pyplot as plt
+# from PIL import Image
+
+
+class DatasetLoader():
+    """Load fixed images, moving images and degrees.
+
+    DataLoader defines basic operations to load data.
+    the data should be organized in the following fashion:
+
+    all according to MATLAB function GenerateDescriminatorData
+    (Paths are supllied in PathFile.txt)
+
+    Path:
+    fixed_image.mat
+    RotateImages
+        -5Degrees.mat
+        -4Degrees.mat
+        ..
+        ..
+        5Degrees.mat
+
+
+    """
+
+    def __init__(self, config_path):
+        """Construct a data loader using a configuration file.
+
+        Args:
+            config_path: path to .json configuration file.
+            see __init__ definition to observe required configurations.
+
+        Returns:
+            a basic DataLoader instance.
+
+        Raises:
+            NameError: if configuration file path can't be found.
+        
+        Todo:
+            add assertions for all parameters.
+        
+        """
+        self.params = utils.import_json_config(config_path)
+        self.crop_size = self.params["crop_size"]
+        self.batch_size = self.params["batch_size"]
+        self.num_of_epochs = self.params["num_of_epochs"]
+        self.channels = self.params["channels"]
+        self.image_size = self.params["image_size"]
+        self.directory = os.getcwd()
+
+    def _single_load(self, _dir):
+        # TODO this function needs some cleaning
+        Rot_list, image_list , degrees_list = self._image_pairs(_dir)
+        Rot_files = np.ndarray([len(Rot_list), self.image_size[0], self.image_size[1], self.channels])
+
+        image_files = np.ndarray([len(image_list), self.image_size[0], self.image_size[1], self.channels])
+        idx = 0
+        for Rot in Rot_files:
+            if os.path.splitext(Rot)[1] == '.mat':
+                Rot_files[idx, :, :, :] = np.expand_dims(scipy.io.loadmat(Rot), axis=2)
+            elif self.channels == 3:
+                pass
+            else:
+                pass
+            idx += 1
+        idx = 0
+        for image in image_list:
+            if os.path.splitext(image)[1] == '.mat':
+                image_files[idx, :, :, :] = np.expand_dims(scipy.io.loadmat(image), axis=2)
+            elif self.channels == 3:
+                pass
+            else:
+                pass
+            idx += 1
+        image_tensor = tf.convert_to_tensor(image_files, dtype=np.float32)
+        # TODO check if the float32 type reduce the resolution
+        Rot_tensor = tf.convert_to_tensor(Rot_files, dtype=np.float32)
+        Degrees_tensor = tf.convert_to_tensor(degrees_list, dtype=np.float32)
+        # ranks_tensor = tf.reshape(ranks_tensor, [-1, 1])
+
+        return image_tensor, Rot_tensor, Degrees_tensor
+
+    def _spatial_augmentation(self, seg, image, rank):
+        # concat seg and image together:
+        batch = tf.concat(axis=2, values=[seg, image])
+        if self.params["crop"]:
+            batch = self._random_crop(batch)
+        if self.params["flip"]:
+            batch = tf.image.random_flip_up_down(batch)
+            batch = tf.image.random_flip_left_right(batch)
+        if self.params["rotate"]:
+            batch = tf.image.rot90(batch, k=np.random.randint(0, 3))
+            # separate seg and image:
+        seg = batch[:, :, 0:self.params["channels"]]
+        image = batch[:, :, self.params["channels"]:2 * self.params["channels"]]
+
+        return seg, image, rank
+
+    def _random_crop(self, batch):
+        return tf.random_crop(batch, [self.crop_size[0], self.crop_size[1], self.params["channels"] * 2])
+
+    @staticmethod
+    def _ranks_reader(ranksdir, index, isdice=True):
+        orig_dir = os.getcwd()
+        os.chdir(ranksdir)
+        ranks_list = []
+
+        if isdice:
+            ranks = utils.import_json_config('dice_ranks_%03.f.json' % index)
+        else:
+            ranks = utils.import_json_config('jaccard_ranks_%03.f.json' % index)
+
+        for rank in ranks:
+            ranks_list.append(ranks[rank])
+
+        os.chdir(orig_dir)
+
+        return ranks_list
+
+    def _image_pairs(self, datadir):
+
+        fix_image_list = []
+        Rot_image_list = []
+        degrees_list = []
+
+        # load image names into a list
+        imagedir = (self.directory + '/%s' % datadir)
+        # TODO think if we don't want to get the absolute dir
+        # in the function call (load function in origin)
+        fix_image_list.append((imagedir + '/fix_image.mat'))
+        current_dir = (imagedir + '/RotateImages')
+        Rot_filenames = os.listdir(current_dir)
+        for Rot_image in Rot_filenames:
+            Rot_image_list.append((current_dir + '/%s' % Rot_image))
+            degrees_name = Rot_image.split("D") #name of rotate files is i.e 3Degrees.mat
+            degrees_list.append = map(int, degrees_name[0])
+         return Rot_image_list, fix_image_list , degrees_list
+
+    def intelligent_load(self, dir_list):
+        """Load images and segmentations intelligently, minimizing memory excess use.
+        
+        TODO: implement!
+        1. single load will produce:
+            (image0, seg00, seg01,..., seg0N, ranks00, ranks01,..., ranks0N)
+            (image1, seg10, seg11,..., seg1N, ranks10, ranks11,..., ranks1N)
+            (             ...               ,          ...                 )
+            (imageM, segM0, segM1,..., segMN, ranksM0, ranksM1,..., ranksMN)
+        2. a dataset will be created for each of the lists.
+        3. every dataset will be shuffled with the same seed, maintaining ordering.
+        4. two new dataset will be created from concatanating all of the segs and all of the ranks.
+        5. the image dataset will go through "repeat", N times..
+        6. the input_fn will recieve the three datasets. it will make one_shot_iterators from every dataset
+        7. we will manually join the get_next() from the images and from the segs datasets into a dictionary,
+            and input_fn will produce:
+            dict("image": image_iter.get_next(), "seg": seg_iter.get_next), rank_iter.get_next(
+        
+        """
+        raise NotImplementedError
+
+    def tile_load(self, dir_list):
+        """Load images and segmentation by repeating the images in memory for each segmentation.
+        
+        Tile load is not an optimal solution since the same data is replicated in the memory.
+        We propose the following solution to be implemented later:
+
+        At the moment we assume #images == #segs/ranks
+        this means that the images are REPLICATED in the memory
+        it is not ideal and better implementation will be to use intelligent_load()
+
+        Args:
+            dir_list: list of directories, each a 'sequence_dir' that is built
+                according to the hierarchy defined in the DatasetLoader class.
+
+        Todo:
+            Currently we support only one dir at a time!
+        
+        """
+        # we assume segs contains all segmentations for images, maintaining ordering
+        # if there are N segs per image, we assume they are at places
+        # (i, M+i, 2M+i... (N-1)*M+i) for image i
+        
+        images, Rots, degrees = self._single_load(dir_list[0])
+        num_images = images.shape.as_list()[0]
+        num_Rot = Rots.shape.as_list()[0]
+
+        images = tf.tile(images, [np.int32(num_Rot / num_images), 1, 1, 1])
+        degrees = tf.reshape(degrees, [-1])
+        # will raise exception if number of images != number of segs
+        return tf.data.Dataset.from_tensor_slices((images, Rots, degrees))
+
+    def shuffle_batch_repeat(self, dataset, batch_size=None, repeat_num=None):
+        """Shuffle, batch and repeat a dataset according to configuration.
+
+        The configuration file defines the number of repeats (num_of_epochs)
+        and the batch size.
+
+        Args:
+            dataset: A Dataset on which to perform the transformations.
+            batch_size: Optional. configures the sizes of the batchs.
+                Default taken from configuration.
+            repeat_num: Optional. configures how many times to repeat the dataset.
+                Default taken from configuration.
+
+        Returns
+            a Dataset.
+        
+        """
+        if batch_size is None:
+            batch_size = self.batch_size
+        if repeat_num is None:
+            repeat_num = self.num_of_epochs
+        
+        # TODO asaf - 128 is an arbitrary number for the buffer size in shuffle
+        dataset = dataset.map(self._spatial_augmentation)
+        return dataset.shuffle(128).batch(batch_size).repeat(repeat_num)
+
+    @staticmethod
+    def input_fn(dataset):
+        """Organize the features and labels to be inserted into the model's model_fn.
+
+        From Tensorflow's Programmer's Guide:
+        A function that constructs the input data for evaluation.
+        The function should construct and return one of the following:
+        A 'tf.data.Dataset' object: Outputs of Dataset object must be a tuple
+            (features, labels) with same constraints as below.
+        A tuple (features, labels): Where features is a Tensor or a dictionary
+            of string feature name to Tensor and labels is a Tensor
+            or a dictionary of string label name to Tensor.
+            Both features and labels are consumed by model_fn.
+            They should satisfy the expectation of model_fn from inputs.
+
+        Args:
+            dataset: A Dataset that produces three elements: images, segs, ranks
+        
+        Returns:
+            a get_next() operation of an Iterator on top of the new mapped dataset,
+                after organizing the data in a (features, labels) tuple.
+        
+        """
+        # change dataset structure to (features, labels)
+        def parser(images, Rots, degrees):
+            features = {"images": images, "Rots": Rots}
+            labels = degrees
+            return features, labels
+
+        dataset = dataset.map(parser)
+        return dataset.make_one_shot_iterator().get_next()
+
+
+def main():
+    pass
+    # """Test the loaders, for debug purposes only."""
+    # # current_dir = os.getcwd()
+
+    # config_path = 'dataLoaderParams.json'
+    # test = DatasetLoader(config_path)
+    # tf.global_variables_initializer()
+    # #seg, image, rank = test.tile_load(['data/TDG/fluo-c2dl-msc'])
+
+    # with tf.Session() as sess:
+
+    #     coord = tf.train.Coordinator()
+    #     threads = tf.train.start_queue_runners(coord=coord)
+    #     seg_batch, image_batch, rank_batch = sess.run([seg, image, rank])
+    #     coord.request_stop()
+    #     coord.join(threads)
+    #     for i in range(0, np.shape(seg_batch)[0]):
+    #         img = np.squeeze(image_batch[i, :, :, :], axis=2)
+    #         seg = np.squeeze(seg_batch[i, :, :, :], axis=2)
+    #         plt.subplot(1, 2, 1)
+    #         plt.imshow(img)
+    #         plt.subplot(1, 2, 2)
+    #         plt.imshow(seg)
+    #         plt.title("Seg Rank: %s" % rank_batch[i])
+    #         plt.show()
+
+
+if __name__ == '__main__':
+    main()
---
+++ models.py
@@ -0,0 +1,297 @@
+import tensorflow as tf
+import utils
+
+
+class _BaseModel:
+    """Define internal ops and required implementations for rib-cage based neural networks.
+
+    The base model parses the configuration file passed to it on initialization,
+    and defines the basic ops of the network such as the activation, the loss,
+    the rib structure and the optimizer.
+
+    """
+
+    def __init__(self, config):
+        """Parse basic configurations for the model.
+
+        Args:
+            config: Configuration file for the network architecture.
+
+        Returns:
+            A basic model instance.
+
+        Raises:
+            AssertionError: if data_format in the configuration is not NHWC.
+            KeyError: if some configuration can not be parsed.
+
+        """
+        self.config = config
+        self.in_training = None
+
+        assert self.config['data_format'] == 'NHWC', \
+            "only NHWC format is supported"
+        if self.config['data_format'] == 'NHWC':
+            self.config['concat_dim'] = 3
+            self.config['batch_norm_axis'] = -1
+        else:
+            self.config['concat_dim'] = 1
+            self.config['batch_norm_axis'] = 1
+
+    def activation(self, inputs):
+        if self.config['activation'] == 'relu':
+            return tf.nn.relu(inputs, name='relu')
+        elif self.config['activation'] == 'leaky_relu':
+            return tf.nn.leaky_relu(
+                inputs, self.config['leaky_relu_alpha'], name='leaky_relu')
+        else:
+            raise ValueError('unknown activation function')
+
+    def basic_rib(self, scope_name, inputs, outputs):
+        in_training = self.in_training
+        conv_kernel_size = self.config['conv_kernel_size']
+        conv_strides = self.config['conv_strides']
+        batch_norm_axis = self.config['batch_norm_axis']
+        pool_size = self.config['pool_size']
+        pool_strides = self.config['pool_strides']
+
+        with tf.variable_scope(scope_name):
+            conv = tf.layers.conv2d(
+                inputs, outputs, conv_kernel_size,
+                conv_strides, name='conv')  # output size: (W-F+2P)/S + 1
+            # TODO asaf - check why epsilon is different than default 1e-3
+            batch_norm = tf.layers.batch_normalization(
+                conv, batch_norm_axis, training=in_training,
+                epsilon=1e-1, name='batch_norm')
+            relu = self.activation(batch_norm)
+            pool = tf.layers.max_pooling2d(
+                relu, pool_size, pool_strides, name='max_pool')
+
+        return pool
+
+    def train_loss_op(self, ranks, predictions, huber_delta=0):
+        if self.config['loss'] == 'huber':
+            return tf.losses.huber_loss(ranks, predictions, delta=huber_delta)
+        else:  # MSE
+            return tf.losses.mean_squared_error(ranks, predictions)
+
+    def mean_single_prediction(self, predictions):
+        """Predict segmentation rank in case all samples are from the same image.
+        
+        In case we sample several crops from the same image, we can take the mean
+        of the predictions as the prediction per one image
+
+        Args:
+            predictions: The predictions tensor for the ranks.
+
+        Returns:
+            A scalar tensor.
+        
+        """
+        return tf.reduce_mean(predictions)
+
+    def optimizer(self):
+        return {
+            'Adam': tf.train.AdamOptimizer(),
+            'RmsProp': tf.train.RMSPropOptimizer(0.001)
+            # Adam is the default optimizer
+        }.get(self.config['optimizer'], tf.train.AdamOptimizer())
+
+    def build(self):
+        raise NotImplementedError
+
+    @staticmethod
+    def model_fn(features, labels, mode, params):
+        """Build the network according the mode of operation and the given inputs.
+
+        Implement according to Tensorflow Custom Estimators Guide:
+        https://www.tensorflow.org/versions/master/api_docs/python/tf/estimator/Estimator
+
+        """
+        raise NotImplementedError
+
+
+class RibCageRegressionNet(_BaseModel):
+    """Define a regression model of ranking segmentations, on a continuous scale from 0 to 1.
+
+    The RibCageRegressionNet is comprised of two main elemnts:
+    
+    rib_cage:
+        A deep sequence of triplets of basic_rib_cage as defined in the BaseModel.
+        The triplets form three deep queues: one for the segmentation frame,
+        one for the image, and one that is a concatenation of the other two.
+    
+    sternum:
+        A deep sequence of dense layers ending in one neuron prediction the rank
+        of the segmentation.
+
+    """
+
+    def __init__(self, config_path):
+        """Parse configurations for the RibCageNet architecture.
+        
+        Args:
+            config_path: Configuration file path for the network architecture.
+
+        Returns:
+            An instance of the RibCageRegressionNet builder.
+
+        Raises:
+            AssertionError: if data_format in the configuration is not NHWC.
+            KeyError: if some configuration can not be parsed.
+            
+        """
+        config = utils.import_json_config(config_path)
+        super(RibCageRegressionNet, self).__init__(config)
+
+    def build(self, features, in_training, reuse):
+        """Build the network according to the configuration.
+
+        Args:
+            features: The inputs to the network, given as a dictionary containing
+                two keys: 'segs' and 'images', each contains the data in NHWC format.
+                obviously, all dimensions must be equal.
+            in_training: A boolean representing the state of the network.
+                Critical to batch_norm and dropout layers.
+            reuse: Reuse flag for reusing weights under the variable scopes
+                of the rib_cage and the sternum.
+        
+        Returns:
+            Segmentation rank predictions tensor for N samples.
+
+        Raises:
+            AssertionError: if sizes of images and segs are not equal.
+
+        """
+        self.in_training = in_training
+        print('RibCageRegressionNet: Model was built with in training=' + str(in_training))
+
+        left_ribs = []
+        right_ribs = []
+        center_ribs = []
+        sternum = []
+
+        segs = features['segs']
+        images = features['images']
+
+        assert(segs.shape.as_list() == images.shape.as_list())
+
+        with tf.variable_scope('rib_cage', reuse=reuse):
+            self._left_input = segs
+            self._right_input = images
+            self._center_input = tf.concat(
+                [self._left_input, self._right_input], self.config['concat_dim'])
+            # rib 0
+            outputs = self.config['conv_filters_list'][0]
+            left_ribs.append(
+                self.basic_rib('left_rib_0', self._left_input, outputs))
+            right_ribs.append(
+                self.basic_rib('right_rib_0', self._right_input, outputs))
+            center_ribs.append(
+                self.basic_rib('center_rib_0', self._center_input, outputs))
+
+            # build list of ribs in a variable legth
+            # and connect each rib's output to the next input
+
+            concat_dim = self.config['concat_dim']
+            for d in range(1, self.config['conv_depth']):
+                outputs = self.config['conv_filters_list'][d]
+                left_ribs.append(
+                    self.basic_rib('left_rib_' + str(d), left_ribs[d - 1], outputs))
+                right_ribs.append(
+                    self.basic_rib('right_rib_' + str(d), right_ribs[d - 1], outputs))
+                concat = tf.concat(
+                    [left_ribs[d - 1], right_ribs[d - 1], center_ribs[d - 1]], concat_dim)
+                center_ribs.append(
+                    self.basic_rib('center_rib_' + str(d), concat, outputs))
+
+        with tf.variable_scope('sternum', reuse=reuse):
+            last_concat = tf.concat(
+                [left_ribs[d], right_ribs[d], center_ribs[d]], concat_dim)
+            dense_input = tf.layers.flatten(last_concat, name="flatten")
+            units = self.config['fc_units_list'][0]
+            sternum.append(
+                tf.layers.dense(dense_input, units, name='fc_0'))
+
+            # build list of dense layers in a variable legth
+            # and connect each layers output to the next input
+
+            for d in range(1, self.config['fc_depth']):
+                units = self.config['fc_units_list'][d]
+                sternum.append(
+                    tf.layers.dense(sternum[d - 1], units, name='fc_' + str(d)))
+            with tf.variable_scope('predictions'):
+                self.predictions = tf.reshape(tf.sigmoid(sternum[d], 'sigmoid'), [-1])
+            return self.predictions
+
+    @staticmethod
+    def model_fn(features, labels, mode, params):
+        """Build the network according the mode of operation and the given inputs.
+
+        The model_fn is used in accordance with the requirments in Tensorflow's
+        Estimators package.
+        
+        From Tensorflow's Programmer's Guide:
+        'The final step in creating a model function is to write branching code
+        that implements prediction, evaluation, and training.
+        The model function gets invoked whenever someone calls the Estimator's
+        train, evaluate, or predict methods.'
+
+        Args:
+            features: This is the first item returned from the input_fn passed
+                to train, evaluate, and predict. This should be a single Tensor or dict of same.
+            labels: This is the second item returned from the input_fn passed
+                to train, evaluate, and predict. This should be a single Tensor or dict of same
+                (for multi-head models). If mode is ModeKeys.PREDICT, labels=None will be passed.
+                If the model_fn's signature does not accept mode,
+                the model_fn must still be able to handle labels=None.
+            mode: Optional. Specifies if this training, evaluation or prediction. See ModeKeys.
+            params: Optional dict of hyperparameters.
+                Will receive what is passed to Estimator in params parameter.
+                This allows to configure Estimators from hyper parameter tuning.
+            config: Optional configuration object.
+                Will receive what is passed to Estimator in config parameter,
+                or the default config. Allows updating things in your model_fn based on configuration
+                such as num_ps_replicas, or model_dir.
+        
+        Returns:
+            EstimatorSpec
+        
+        """
+        # parse params:
+        config_path = params['config_path']
+
+        model = RibCageRegressionNet(config_path)
+        with tf.variable_scope('RibCageRegressionNet'):
+            train_predictions = model.build(features, in_training=True, reuse=False)
+            test_predictions = model.build(features, in_training=False, reuse=True)
+
+        if mode == tf.estimator.ModeKeys.PREDICT:
+            return tf.estimator.EstimatorSpec(mode, predictions=test_predictions)
+        # Create loss and optimizer
+        train_loss_op = model.train_loss_op(labels, train_predictions)
+        optimizer = model.optimizer()
+
+        # from: https://www.tensorflow.org/api_docs/python/tf/layers/batch_normalization
+        # when training, the moving_mean and moving_variance need to be updated.
+        # By default the update ops are placed in tf.GraphKeys.UPDATE_OPS,
+        # so they need to be added as a dependency to the train_op:
+
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+            train_op = optimizer.minimize(train_loss_op, global_step=tf.train.get_global_step())
+
+        # Create logging hooks
+        log_hook = tf.train.LoggingTensorHook({'loss': train_loss_op}, every_n_iter=100)
+
+        return tf.estimator.EstimatorSpec(
+            mode=mode,
+            predictions=train_predictions,
+            loss=train_loss_op,
+            train_op=train_op,
+            training_hooks=[log_hook]
+        )
+
+
+if __name__ == '__main__':
+    from main import main
+    main()
