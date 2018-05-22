b4833a8671098bd43e3a6097226d8f396c478fed
YradenRavid <39432202+YradenRavid@users.noreply.github.com>
2018-05-22 17:23:20 +0300

    Add files via upload

--- GenerateDescriminatorData.m
+++ GenerateDescriminatorData.m
@@ -12,6 +12,10 @@ function  GenerateDescriminatorData(InputPath,OutputPath,brains,Slices,RangeOfDe
 % Slices - vector: what slices do we take?
 % RangeOfDegrees - vector that indicates the range orf degrees. i.e
 % -5:0.5:5
+
+%  label - for RangeOfDegrees = -5:5 label will be -7:7 with score detailed
+%  in function below. for example for -5 degrees label will be a vector - 
+% [0.05 0.15 0.6 0.15 0.05 0 0 0 0 0 0 0 0 0 0]
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 addpath('C:\Users\ירדן רביד\Desktop\MRI'); %where nii library is
@@ -25,6 +29,9 @@ for i=1:numel(RangeOfDegrees)
     Rotate = [cos(teta) sin(teta) 0 ; -sin(teta) cos(teta) 0 ; 0 0 1]; 
     tform(i) = affine2d(Rotate);
 end
+
+label_array = zeros(numel(RangeOfDegrees)+4,1);
+delta_degree = diff(RangeOfDegrees(1:2));
 
 fid = fopen('PathFile.txt','w');
 
@@ -44,11 +51,23 @@ for i=brains
         fix_image=I(:,:,Slices(j));
         save([currentPath '\fix_image'],'fix_image'); 
         mkdir([currentPath '\RotateImages'])
-        currentPath = [currentPath '\RotateImages'];
+        currentPath1 = [currentPath '\RotateImages'];
+        mkdir([currentPath '\labels'])
+        currentPath2 = [currentPath '\labels'];
         for h=1:numel(RangeOfDegrees)
             Tform = tform(h);
             rotate_image = imwarp(fix_image, Tform , 'OutputView', imref2d(size(fix_image)));
-            save([currentPath '\' num2str(RangeOfDegrees(h)) 'Degrees'], 'rotate_image');
+            save([currentPath1 '\' num2str(RangeOfDegrees(h)) 'Degrees'], 'rotate_image');
+        end
+        for h=1:numel(RangeOfDegrees)
+            true_val_place = (RangeOfDegrees(h) - RangeOfDegrees(1))/delta_degree +3;
+            label_array(true_val_place) = 0.6;
+            label_array(true_val_place-1) = 0.15;
+            label_array(true_val_place+1) = 0.15;
+            label_array(true_val_place-2) = 0.05;
+            label_array(true_val_place+2) = 0.05;
+            save([currentPath2 '\' num2str(RangeOfDegrees(h)) 'label'], 'label_array');
+            label_array = zeros(numel(RangeOfDegrees)+4,1);
         end
     end
 end
